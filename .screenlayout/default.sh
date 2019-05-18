6f4cf80ea5687f8a4380e101388d3952614dcaa1
JayDee <jonerik84@gmail.com>
2019-05-18 15:06:04 +0200

    Monitor change & replace feh with Nitrogen

--- .screenlayout/default.sh
+++ .screenlayout/default.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-xrandr --output DVI-D-0 --primary --mode 1920x1080 --rate 144 --pos 1920x1080 --rotate normal --output HDMI-1-2 --off --output HDMI-1-1 --mode 1920x1080 --rate 60 --pos 1920x0 --rotate normal --output HDMI-1 --off --output HDMI-0 --off --output DP-3 --off --output DP-2 --mode 1920x1080 --rate 144 --pos 0x1080 --rotate normal --output DP-1 --off --output DP-0 --mode 1920x1080 --rate 144 --pos 3840x1080 --rotate normal
+xrandr --output DVI-D-0 --primary --mode 1920x1080 --rate 144 --pos 1920x1080 --rotate normal --output HDMI-1-2 --off --output HDMI-1 --mode 1920x1080 --rate 60 --pos 1920x0 --rotate normal --output HDMI-1 --off --output HDMI-0 --off --output DP-3 --off --output DP-2 --mode 1920x1080 --rate 144 --pos 0x1080 --rotate normal --output DP-1 --off --output DP-0 --mode 1920x1080 --rate 144 --pos 3840x1080 --rotate normal
 
 xrandr --dpi 92
