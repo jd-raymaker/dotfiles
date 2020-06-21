6ea348cbcf4522b9a21fb5f5bd4ef2c31f598581
JD <jd@jaydee.systems>
2020-06-21 13:48:57 +0200

    update xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -11,6 +11,9 @@ import XMonad.Layout.Spacing
 import XMonad.Util.EZConfig (additionalKeysP)
 import XMonad.Util.Run (spawnPipe)
 import XMonad.Util.SpawnOnce
+import Data.Monoid
+import qualified XMonad.StackSet as W
+import qualified XMonad.Layout.ToggleLayouts as T (toggleLayouts, ToggleLayout(Toggle))
 
 ------------------
 -- Config
@@ -32,17 +35,32 @@ myLayoutHook = spacingRaw True (Border 5 5 5 5) True (Border 5 5 5 5) True $ lay
 -- Autostart
 -----------------
 myStartupHook =	do
-	spawnOnce "compton &"
+	spawnOnce "picom &"
 	spawnOnce "nitrogen --restore &"
+
+-----------------
+-- ManageHook
+-- --------------
+myManageHook :: XMonad.Query (Data.Monoid.Endo WindowSet)
+myManageHook = composeAll
+	[ className =? "vlc" 	--> doFloat
+	, className =? "mpv" 	--> doFloat
+	, className =? "Gimp" 	--> doFloat
+	, (className =? "firefox" <&&> resource =? "Dialog") 	--> doFloat
+	]
 
 -----------------
 -- Keybindings
 -----------------
-myKeys = [ ("M-C-r", spawn "xmonad --recompile") 	-- Recompile xmonad
-	 , ("M-S-r", spawn "xmonad --restart") 		-- Restart xmonad
-	 , ("M-S-e", io exitSuccess) 			-- Quit xmonad
-	 , ("M-<Return>", spawn myTerminal) 		-- Spawn terminal
-	 , ("M-q", kill1) 				-- Kill focused
+myKeys = [ ("M-C-r", spawn "xmonad --recompile") 		-- Recompile xmonad
+	 , ("M-S-r", spawn "xmonad --restart") 			-- Restart xmonad
+	 , ("M-S-e", io exitSuccess) 				-- Quit xmonad
+	 , ("M-<Return>", spawn myTerminal) 			-- Spawn terminal
+	 , ("M-q", kill1) 					-- Kill focused
+	 , ("M-<Tab>", spawn "rofi -show run") 			-- Open rofi 
+	 , ("M-a", spawn (myTerminal ++ " -e ranger"))
+	 , ("M-f", sendMessage (T.Toggle "floats")) 		-- Set to float
+	 , ("M-S-f", withFocused $ windows . W.sink) 		-- Push to tile
 	 ]
 
 ----------------
@@ -51,7 +69,7 @@ myKeys = [ ("M-C-r", spawn "xmonad --recompile") 	-- Recompile xmonad
 main = do
 	xmproc <- spawnPipe "xmobar -x 0 $HOME/.config/xmobar/xmobarrc"
 	xmonad $ docks def 
-		 { manageHook = manageDocks <+> manageHook defaultConfig
+		 { manageHook = manageDocks <+> myManageHook
 		 , layoutHook = avoidStruts $ myLayoutHook 
 		 , borderWidth 		= myBorderWidth
 		 , terminal 		= myTerminal
