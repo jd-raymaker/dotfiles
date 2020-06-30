0c31bc64d52c777e8ffa1b0e595f76795ce40e40
JayDee <jd@jaydee.systems>
2020-06-30 12:50:31 +0200

    changes for my new system

--- .screenlayout/default.sh
+++ .screenlayout/default.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-xrandr --output DVI-D-0 --primary --mode 1920x1080 --rate 144 --pos 1920x1080 --rotate normal --output HDMI-1-2 --off --output HDMI-1 --mode 1920x1080 --rate 60 --pos 1920x0 --rotate normal --output HDMI-1 --off --output HDMI-0 --off --output DP-3 --off --output DP-2 --mode 1920x1080 --rate 144 --pos 0x1080 --rotate normal --output DP-1 --off --output DP-0 --mode 1920x1080 --rate 144 --pos 3840x1080 --rotate normal
-
-xrandr --dpi 92
+xrandr --output DP-0 --mode 1920x1080 --pos 3840x0 --rotate normal --output DP-1 --off --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-5 --off
+xrandr --output DP-4 --mode 1920x1080 --rate 144
