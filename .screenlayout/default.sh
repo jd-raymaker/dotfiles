bc5630e683a77e760123c4bff50a43878afd3006
JayDee <jd@jaydee.systems>
2020-07-09 20:47:57 +0200

    monitor refresh rate fix

--- .screenlayout/default.sh
+++ .screenlayout/default.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-xrandr --output DP-0 --mode 1920x1080 --pos 3840x0 --rotate normal --output DP-1 --off --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-5 --off
-xrandr --output DP-4 --mode 1920x1080 --rate 144
+xrandr --output DP-0 --mode 1920x1080 --rate 144 --pos 3840x0 --rotate normal --output DP-1 --off --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --primary --rate 144 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-5 --off
