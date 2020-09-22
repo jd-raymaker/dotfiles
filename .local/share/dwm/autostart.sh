3d008dc72fab26ce9a2122bbc5e4470cc728f13a
JayDee <jd@jaydee.systems>
2020-09-22 19:06:08 +0200

    sorting

--- .local/share/dwm/autostart.sh
+++ .local/share/dwm/autostart.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-nitrogen --restore &
+
+echo "Starting dunst"
 dunst &
+echo "Starting lxsession"
 lxsession &
+echo "Restoring nitrogen wallpaper"
+nitrogen --restore &> /dev/null
+echo "Loading nvidia settings"
 nvidia-settings -l &
+
+# Run pulseaudio as daemon if no pid is returned
+echo "Fixing pulseaudio daemon"
+pidof pulseaudio || pulseaudio -D &
