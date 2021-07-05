aaf2f225ac780ba240e4a9cd739d043934759ad6
JayDee Raymaker <jd@jaydee.systems>
2021-07-06 00:37:19 +0200

    Update autostart.sh

--- .local/share/dwm/autostart.sh
+++ .local/share/dwm/autostart.sh
@@ -2,12 +2,10 @@
 
 dunst &
 lxsession &
-nitrogen --restore &> /dev/null
+nitrogen --restore & 
 nvidia-settings -l &
 udiskie -s &
-
-# Run pulseaudio as daemon if no pid is returned
-pidof pulseaudio || pulseaudio -D &
+xcompmgr -c -C -n &
 
 # Run status bar
 bash $HOME/.local/share/dwm/dwm_status &
