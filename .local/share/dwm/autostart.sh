fb1d85062f146ca17be50d366574f7ba7ab3d6fe
JayDee Raymaker <16980904+jd-raymaker@users.noreply.github.com>
2023-12-03 03:05:22 +0100

    update autostart

--- .local/share/dwm/autostart.sh
+++ .local/share/dwm/autostart.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 dunst &
-lxsession -a &
-nitrogen --restore & 
+lxsession &
+nitrogen --restore &
 numlockx &
 nvidia-settings -l &
 udiskie -s &
 xcompmgr -c -C -n &
-sh /$HOME/.config/conky/launch.sh &
 
 # Run status bar
 bash $HOME/.local/share/dwm/dwm_status &
